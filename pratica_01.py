83f39d4d5d35db5773f9c6612b6381d992c7c1da
Daniel-Alves <assistenciatecnica@ballke.com.br>
2025-10-20 17:42:46 -0300

    fim da primeira aula

---
+++ pratica_01.py
@@ -0,0 +1,10 @@
+print('Python na Escola de Programação da Alura')
+
+nome = 'Daniel'
+idade = 28
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+print('A\nL\nU\nR\nA')
+
+import math
+print(f'O valor de pi é aproximadamente {math.pi:.2f}')
