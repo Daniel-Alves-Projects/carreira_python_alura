479baa13506f16af4bfc25a8996a4f8b1829f545
Daniel-Alves <assistenciatecnica@ballke.com.br>
2025-10-21 11:31:34 -0300

    aula 2 Hora da prática

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-print("""
+import os
+def exibir_nome_do_programa():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -6,12 +8,35 @@ print("""
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░""")
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+def finalizar_app():
+    os.system('clear')
+    print('Finalizando o app')
 
-print(f'Você escolheu a opção : {opcao_escolhida}')
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
 
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurante')
+
+    elif opcao_escolhida == 2:
+        print('Listar Restaurantes')
+
+    elif opcao_escolhida == 3:
+        print('Ativar Restaurante')
+
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
