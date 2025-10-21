93316edec53eaeb9f7502e8e076c993f2f17ade4
Daniel-Alves <assistenciatecnica@ballke.com.br>
2025-10-21 13:43:21 -0300

    ex004

--- exercicios/ex004.py
+++ exercicios/ex004.py
@@ -1,2 +1,13 @@
-x = int(input('Digite o eixo x'))
-y = int(input('Digite o eixo y'))
+x = float(input("Digite a coordenada x: "))
+y = float(input("Digite a coordenada y: "))
+
+if x > 0 and y > 0:
+    print("O ponto está no primeiro quadrante.")
+elif x < 0 and y > 0:
+    print("O ponto está no segundo quadrante.")
+elif x < 0 and y < 0:
+    print("O ponto está no terceiro quadrante.")
+elif x > 0 and y < 0:
+    print("O ponto está no quarto quadrante.")
+else:
+    print("O ponto está sobre um eixo ou na origem.")
